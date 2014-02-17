fbc4f9ac2c7e6fc2163f79dad72d42a82a293eee
stinger907 <stinger907@yahoo.com>
2014-02-17 10:18:12 +0200

    initial commit

---
+++ AlexandreDumasOOP/AlexandreDumasOOP.UI/GameUI.cs
@@ -0,0 +1,14 @@
+﻿using AlexandreDumasOOP.Common;
+using System;
+using System.Linq;
+
+namespace AlexandreDumasOOP.UI
+{
+    class GameUI
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
